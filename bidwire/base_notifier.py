b96acbe11a4f8d4ef247594ce754596a26718e77
Sumeet <teemus@gmail.com>
2017-04-18 22:17:00 -0400

    Change recipients list to use env variable

--- bidwire/base_notifier.py
+++ bidwire/base_notifier.py
@@ -1,6 +1,7 @@
 import logging
 import os
 import sendgrid
+import bidwire_settings
 from sendgrid.helpers.mail import *
 from yattag import Doc
 
@@ -13,7 +14,7 @@ ITEMS_DELIMITER = " ### "
 
 class BaseNotifier:
     # type is derived from the return of get_site
-    def __init__(self, recipients):
+    def __init__(self, recipients = bidwire_settings.EMAIL_RECIPIENTS):
         self.type = self.get_site().value
         self.recipients = recipients
 
@@ -54,7 +55,7 @@ class BaseNotifier:
             to_email = Email(recipient)
             mail = Mail(from_email, subject, to_email, content)
             response = sg.client.mail.send.post(request_body=mail.get())
-        self.send_debug_email(sg, from_email, bids)
+        # self.send_debug_email(sg, from_email, bids)
 
     def send_debug_email(self, sendgrid_client, from_email, bids):
         subject = "{} Scraping Status".format(self.type)
--- bidwire/commbuys_notifier.py
+++ bidwire/commbuys_notifier.py
@@ -1,12 +1,8 @@
 from base_notifier import BaseNotifier
 from bid import Bid
-import bidwire_settings
 
 
 class CommBuysNotifier(BaseNotifier):
-    def __init__(self):
-        BaseNotifier.__init__(self, bidwire_settings.EMAIL_RECIPIENTS)
-
     def get_site(self):
         return Bid.Site.COMMBUYS
 
