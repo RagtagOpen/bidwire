1ce2ebd758b55b7c2bbfebcefd7cff935850058e
Ana Ulin <ana@ulin.org>
2017-05-22 00:22:59 -0700

    Add automatic running of all migrations to Heroku deploy script.

--- heroku-deploy.sh
+++ heroku-deploy.sh
@@ -1,9 +1,12 @@
 #!/bin/bash -e
 
 # Script to sync to master branch and push results to Heroku
+# Then run setup.sh script to make sure all migrations are applied.
+#
 # Assumes that a Heroku remote is configured. See
 # https://devcenter.heroku.com/articles/git for details.
 
 git checkout master
 git pull origin
 git push heroku master
+./setup.sh
