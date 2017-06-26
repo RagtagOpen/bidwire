79eac1e7f5bbf7b5725b70a006becc756f03458d
Zachary Spector <zacharyspector@gmail.com>
2017-06-25 21:14:12 -0400

    Revert changes to original Boston notifier

--- bidwire/notifiers/boston_bid_notifier.py
+++ bidwire/notifiers/boston_bid_notifier.py
@@ -1,6 +1,7 @@
-from .notifier_utils import make_bid_item_body, make_doc_item_body
+from .notifier_utils import make_bid_item_body, make_notice_item_body
 from .base_notifier import BaseNotifier
 from bid import Bid
+from notice import Notice
 
 
 class CityOfBostonNotifier(BaseNotifier):
@@ -14,3 +15,15 @@ class CityOfBostonNotifier(BaseNotifier):
 
     def make_item_body(self, item):
         return make_bid_item_body(item)
+
+
+class CityOfBostonNoticeNotifier(BaseNotifier):
+    def get_site(self):
+        return Notice.Site.BOSTON
+
+    def make_item_body(self, item):
+        assert isinstance(item, Notice)
+        return make_notice_item_body(item)
+
+    def get_listings_pre_text(self, items_length):
+        return "{} new notices".format(items_length)
